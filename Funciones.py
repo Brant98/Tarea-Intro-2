da2cd9b12c8254f63d120695fc4820291cc46f96
tomas100h <tomas100h@gmail.com>
2016-08-21 17:45:57 -0300

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -1,2 +1,13 @@
-# Tarea-Intro-2
-def rating(a, b):
+def rating(id_pelicula):
+    usuarios=0
+    suma_rating=0
+    arch=open('ratings.dat')
+    for linea in arch:
+        linea=linea.strip().split("::")
+        if linea[1]==id_pelicula:
+            suma_rating+=float(linea[2])
+            usuarios+=1
+    arch.close()
+    return round((suma_rating/usuarios),1)
+
+print rating('122')
